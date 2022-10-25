5facb486b333a5321946552aa9eabfbf827cdb7e
Anastasia Kotelnikova <xtlbyhfrjnxfuty@gmail.com>
2022-10-25 16:55:05 +0300

    адаптивная верстка сделана

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
 import '../components';
+
+const applicantForm = document.getElementById('my-form')
+
+function serializeForm(formNode) {
+    return new FormData(formNode)
+}
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+    serializeForm(applicantForm);
+   alert('форма отправлена');
+}
+
+applicantForm.addEventListener('submit', handleFormSubmit)
